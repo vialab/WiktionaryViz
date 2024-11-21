0664cad8ea589dfd4500bc7ea3ed36c6b27d4e78
SethCohen <sethcohen.dev@gmail.com>
2024-11-20 22:02:02 -0500

    feat(README.md): update project name and add badges for website, repo size, and license for better visibility refactor(package.json): rename project to wiktionary-viz and add homepage, predeploy, and deploy scripts for GitHub Pages feat(webpack.config.js): configure publicPath for GitHub Pages and add CopyWebpackPlugin to manage static assets and HTML file

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# WiktionaryViz
+# [WiktionaryViz](https://vialab.github.io/WiktionaryViz/ "vialab.github.io/WiktionaryViz")
 
 An explorative visual analytics tool for linguistics based upon Wiktionary
+
+[![Website](https://img.shields.io/website?label=vialab.github.io/WiktionaryViz&style=for-the-badge&url=https%3A%2F%2Fvialab.github.io/WiktionaryViz)](https://vialab.github.io/WiktionaryViz/)
+![GitHub repo size](https://img.shields.io/github/repo-size/vialab/WiktionaryViz?style=for-the-badge)
+![GitHub](https://img.shields.io/github/license/vialab/WiktionaryViz?style=for-the-badge)
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,22 @@
 const path = require('path');
 const webpack = require('webpack');
 const Dotenv = require('dotenv-webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.js', // Main JavaScript entry point
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        publicPath: '/WiktionaryViz/', // Necessary for GitHub Pages
     },
     resolve: {
         fallback: {
-            "buffer": require.resolve("buffer/"),
-            "stream": require.resolve("stream-browserify"),
-            "crypto": require.resolve("crypto-browserify"),
-            "path": require.resolve("path-browserify"),
-            "os": require.resolve("os-browserify/browser"),
+            buffer: require.resolve('buffer/'),
+            stream: require.resolve('stream-browserify'),
+            crypto: require.resolve('crypto-browserify'),
+            path: require.resolve('path-browserify'),
+            os: require.resolve('os-browserify/browser'),
         },
     },
     plugins: [
@@ -22,6 +24,12 @@ module.exports = {
             Buffer: ['buffer', 'Buffer'],
         }),
         new Dotenv(),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'public', to: '.', globOptions: { ignore: ['**/index.html'] } },
+                { from: 'public/index.html', to: 'index.html' },
+            ],
+        }),
     ],
     module: {
         rules: [
@@ -35,6 +43,10 @@ module.exports = {
                     },
                 },
             },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
         ],
     },
     devServer: {
@@ -42,5 +54,6 @@ module.exports = {
             directory: path.join(__dirname, 'public'),
         },
         port: 8080,
+        historyApiFallback: true,
     },
 };
