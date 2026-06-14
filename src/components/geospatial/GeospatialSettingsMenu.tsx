b72f8955351cb47314209ac5f0d9a5c2c6f6f080
SethCohen <sethcohen.dev@gmail.com>
2026-06-14 02:57:01 -0400

    chore(geospatial): remove unused mapInstance and map variable imports to clean up code

--- src/components/geospatial/GeospatialSettingsMenu.tsx
+++ src/components/geospatial/GeospatialSettingsMenu.tsx
@@ -20,7 +20,6 @@ const GeospatialSettingsMenu: React.FC<GeospatialSettingsMenuProps> = ({
   lineage,
   word,
   language,
-  mapInstance,
   theme = 'dark',
 }) => {
   const isLight = theme === 'light'
